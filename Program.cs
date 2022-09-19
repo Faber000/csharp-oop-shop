f28736b4a97b7923a6f2343c4f74b54971526598
Fabio Moro <fabio.moro2000@gmail.com>
2022-09-19 17:17:05 +0200

    arrotondamento prezzo

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿
 
-Prodotto prodotto = new Prodotto("iphone 14", "iphone di ultima generazione", 999.99, 99.99);
+Prodotto prodotto = new Prodotto("iphone 14", "iphone di ultima generazione", 999.99);
 prodotto.SetNome("Iphone 14 Pro");
 Console.WriteLine("il prodotto " + prodotto.GetNome() + " avente codice " +prodotto.GetCodice()+" costa " + prodotto.GetPrezzoIva() + "$ Iva compresa");
 Console.WriteLine("descrizione prodotto num. " + prodotto.GetCodiceLungo() + ": " + prodotto.GetDescrizione());
